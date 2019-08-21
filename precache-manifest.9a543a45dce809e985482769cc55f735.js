f5586d08875b30d24190db7505a964553ccc9cc2
shehbazjafri <galaxytwirls@gmail.com>
2019-08-21 15:29:19 +0530

    Updates

---
+++ precache-manifest.9a543a45dce809e985482769cc55f735.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "293e803a4dca29e03725c9f7d46536e2",
+    "url": "/simple-notes/index.html"
+  },
+  {
+    "revision": "fa700234e805cf9a2060",
+    "url": "/simple-notes/static/css/main.1417bbf5.chunk.css"
+  },
+  {
+    "revision": "361b379cf6de1e42d857",
+    "url": "/simple-notes/static/js/2.cd8af363.chunk.js"
+  },
+  {
+    "revision": "fa700234e805cf9a2060",
+    "url": "/simple-notes/static/js/main.3c424309.chunk.js"
+  },
+  {
+    "revision": "be2b0ab370f57903eb2e",
+    "url": "/simple-notes/static/js/runtime~main.16c42d7b.js"
+  },
+  {
+    "revision": "235701a76d9b58419e4096c2fa13c4f7",
+    "url": "/simple-notes/static/media/check.235701a7.svg"
+  },
+  {
+    "revision": "45b30f6af163a69443f70b9e363a9a6e",
+    "url": "/simple-notes/static/media/create.45b30f6a.svg"
+  }
+]);
